43cb515d00a8d09dd6f73a296f17a0122e7a4735
Joey <joeyliuliongwah@gmail.com>
2024-07-11 15:33:07 +0400

    single arg implementation
    Still not printing in editor, problem in execute function. Wrong argument is getting passed

--- LiveFromDAP/src/livefromdap/runner/go_runner.go
+++ LiveFromDAP/src/livefromdap/runner/go_runner.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"plugin"
-	"reflect"
 )
 var importFile *plugin.Plugin
 
 // LoadPlugin loads the specified shared library and assigns it to importFile
-func LoadPlugin(path string) error {
+func loadPlugin(path string) error {
 	var err error
 	importFile, err = plugin.Open(path)
 	if err != nil {
@@ -18,36 +17,33 @@ func LoadPlugin(path string) error {
 }
 
 // LookupSymbol looks up a symbol (function) in the loaded plugin.
-func callPluginFunction(funcName string, args ...interface{}) ([]reflect.Value, error) {
+func callPluginFunction(funcName string, arg int) (int, error) {
 	// Lookup the symbol
 	symbol, err := importFile.Lookup(funcName)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
-	symbolValue := reflect.ValueOf(symbol)
-
-	reflectArgs := make([]reflect.Value, len(args))
-	for i, arg := range args {
-		reflectArgs[i] = reflect.ValueOf(arg)
+	function, ok := symbol.(func(int) int)
+	if !ok {
+		return 0, fmt.Errorf("symbol is not of type func(int) int")
 	}
 
-	result := symbolValue.Call(reflectArgs)
-
+	result := function(arg)
 	return result, nil
 }
 func main() {
-	pluginPath := "/code/src/webdemo/tmp/tmp.so"
-
-	err := LoadPlugin(pluginPath)
-	if err != nil {
-		fmt.Println("Error loading plugin:", err)
-		return
+	for {
+		pluginPath := "/code/src/webdemo/tmp/tmp.so"
+		err := loadPlugin(pluginPath)
+		if err != nil {
+			fmt.Println("Error loading plugin:", err)
+			return
+		}
+		fooResult, err := callPluginFunction("Foo", 1)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Foo(arg) = %v\n", fooResult)
 	}
-
-	fooResult, err := callPluginFunction("Foo", 3, 4)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Foo(3, 4) = %v\n", fooResult[0].Interface())
 }
